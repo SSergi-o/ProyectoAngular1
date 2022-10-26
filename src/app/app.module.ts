97e7e0ec314f27ea910353e5fe4a8311da3746a3
Sergio Guachalla <sergioguachalla0@gmail.com>
2022-10-26 17:03:14 -0400

    Finalización del componente info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SlideshowComponent
+    SlideshowComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule
