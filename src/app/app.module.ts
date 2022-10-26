6166611869c454584a97256325362a20e0ad17cd
Sergio Guachalla <sergioguachalla0@gmail.com>
2022-10-26 17:16:36 -0400

    Finalización proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { InfoComponent } from './info/info.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SlideshowComponent,
-    InfoComponent
+    InfoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
