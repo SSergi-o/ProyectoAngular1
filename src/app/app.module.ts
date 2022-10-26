092516b25370adbba4b3bf349cf595b7da1290c5
Sergio Guachalla <sergioguachalla0@gmail.com>
2022-10-25 20:17:21 -0400

    Creación del componente slideshow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SlideshowComponent
   ],
   imports: [
     BrowserModule
